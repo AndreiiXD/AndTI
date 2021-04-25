dba73442b2596e8135bb4d17ee28e3cd8250a2fe
andre <andrei2000_2010@mail.ru>
2021-04-25 12:36:05 +0300

    Initial commit

---
+++ src/T1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter celsius");
+        double celsius = input.nextDouble();
+        double fahrenheit = (9.0/5)*celsius+32;
+        System.out.println("Fahrenheit: "+fahrenheit);
+
+    }
+}
+
+    © 2021 GitHub, Inc.
+            Terms
